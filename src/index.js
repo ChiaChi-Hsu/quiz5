c5785628b36be9cd11dcc8c9c1b04f0bfd9cffa3
ChiaChi-Hsu <plus7kiki926@gmail.com>
2021-05-05 16:48:54 +0800

    quiz5

--- src/index.js
+++ src/index.js
@@ -4,14 +4,31 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class CounterButton extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      counter: 0
+    }
+    this.handleClick=this.handleClick.bind(this)
+  }
+  handleClick(event){
+    this.setState({counter: this.state.counter+1});
+    console.log(this.state);
+  }
+  render(){
+    var text=this.state.counter;
+    return(
+      <button onClick={this.handleClick}>
+        你點了{text}次。
+      </button>
+    );
+  }
+}
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CounterButton />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
